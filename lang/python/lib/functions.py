45217dea2d1e709516dc474703f9b2bb8c66b407
Thibault Tostain <82095000+ThibaultTostain@users.noreply.github.com>
2025-11-06 21:59:29 +0100

    add more functions to compute if a number is prime or not

--- lang/python/lib/functions.py
+++ lang/python/lib/functions.py
@@ -1,3 +1,29 @@
+def every_number(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, n):
+        if n % i == 0:
+            return False
+    return True
+
+def square_iterator(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 2
+    while i*i < n :
+        if n % i == 0:
+            return False
+        i+=1
+    return True
+
 def square_root_ceil(n):
     if n <= 1:
         return False
--- lang/python/main.py
+++ lang/python/main.py
@@ -3,5 +3,9 @@ from lib import functions, compute
 
 for i in range(100000,100005):
     print("")
+    out, sec = compute.compute(functions.every_number, i)
+    print(f"{i} : {out} in {sec:.6f}s")
+    out, sec = compute.compute(functions.square_iterator, i)
+    print(f"{i} : {out} in {sec:.6f}s")
     out, sec = compute.compute(functions.square_root_ceil, i)
     print(f"{i} : {out} in {sec:.6f}s")

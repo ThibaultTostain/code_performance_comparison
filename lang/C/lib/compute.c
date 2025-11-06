9c399e918c03c6cbab8629bc6bacfa77c208869a
Thibault Tostain <82095000+ThibaultTostain@users.noreply.github.com>
2025-11-06 14:51:55 +0100

    add linear interpolation

--- lang/C/lib/compute.c
+++ lang/C/lib/compute.c
@@ -7,7 +7,29 @@ void compute(int (*evaluated_function)(unsigned long long int n), parameters par
 {
     if (strcmp(parm.type, "linear") == 0)
     {
-        printf("You chose linear !\n");
+        unsigned long long int step = (parm.end - parm.start) / (parm.divisor);
+        for (unsigned long long int value = parm.start; value < parm.end; value += step)
+        {
+            clock_t start_time = clock();
+            int res = 0;
+            for (unsigned int rep = 0; rep < parm.rep; rep++)
+            {
+                res = evaluated_function(value);
+            }
+            clock_t end_time = clock();
+            double elapsed = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+            double avg_time = elapsed / parm.rep;
+            // printf("%d | %llu : %.6fs\n", res, value, avg_time); // Disable to reduce run time
+            if (res == 0)
+            {
+                database_add(db, value, avg_time);
+            }
+            if (avg_time >= parm.max_time)
+            {
+                printf("STOP : avg_time >= MAX_TIME.\n");
+                value = parm.end;
+            }
+        }
     }
     else if (strcmp(parm.type, "log10") == 0)
     {
